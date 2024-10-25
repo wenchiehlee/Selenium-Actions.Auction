4f344d8a3beeef0d26213821db908f1ffd0b0006
wenchiehlee <wjlee@csie.nctu.edu.tw>
2024-10-25 16:06:24 +0800

    make it works for "https://www.twse.com.tw/zh/announcement/auction.html" in windows..

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -1,40 +1,74 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import chromedriver_autoinstaller
-from pyvirtualdisplay import Display
-display = Display(visible=0, size=(800, 800))  
-display.start()
 
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-                                      # and if it doesn't exist, download it automatically,
+import time
+import os
+from os.path import exists
+import shutil
+
+#from pyvirtualdisplay import Display
+#display = Display(visible=0, size=(800, 800))  
+#display.start()
+
+#chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+#                                      # and if it doesn't exist, download it automatically,
                                       # then add chromedriver to path
 
-chrome_options = webdriver.ChromeOptions()    
-# Add your options as needed    
-options = [
-  # Define window size here
-   "--window-size=1200,1200",
-    "--ignore-certificate-errors"
- 
-    #"--headless",
-    #"--disable-gpu",
-    #"--window-size=1920,1200",
-    #"--ignore-certificate-errors",
-    #"--disable-extensions",
-    #"--no-sandbox",
-    #"--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
-]
 
-for option in options:
-    chrome_options.add_argument(option)
+def getDownLoadedFileNameClose():
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+def getDownLoadedFileName():
+    driver.execute_script("window.open()")
+    driver.switch_to.window(driver.window_handles[-1])
+    driver.get('chrome://downloads')
+    #driver.get_screenshot_as_file("page.png")
+    return driver.execute_script("return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('div#content  #file-link').text")
+ 
+  
+downloadDir = f"{os.getcwd()}\\"
+print("Download path=",downloadDir)
+preferences = {"download.default_directory": downloadDir,
+                "download.prompt_for_download": False,
+                "directory_upgrade": True,
+                "safebrowsing.enabled": True}
+chrome_options = webdriver.ChromeOptions()  
+
+chrome_options.add_experimental_option("prefs", preferences)
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+chrome_options.add_argument("--window-size=1200,1200")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--ignore-certificate-errors")
+chrome_options.add_argument("--no-sandbox")
 
     
 driver = webdriver.Chrome(options = chrome_options)
 
-driver.get('http://github.com')
-print(driver.title)
-with open('./GitHub_Action_Results.txt', 'w') as f:
-    f.write(f"This was written with a GitHub action {driver.title}")
+#driver.get('http://github.com')
+#print(driver.title)
+#with open('./GitHub_Action_Results.txt', 'w') as f:
+#    f.write(f"This was written with a GitHub action {driver.title}")
+
+driver.get('https://www.twse.com.tw/zh/announcement/auction.html')
+element = driver.find_element(by=By.CLASS_NAME, value="csv")
+
+element.click()
+time.sleep(2)
+
+latestDownloadedFileName = getDownLoadedFileName() 
+time.sleep(2)
+getDownLoadedFileNameClose()
+DownloadedFilename=''.join(latestDownloadedFileName).encode().decode("utf-8")
+
+if DownloadedFilename != "auction.csv":
+    # Copy the file to "auction.csv"
+    shutil.copy(DownloadedFilename, "auction.csv")
+    print(f"File '{DownloadedFilename}' copied to 'auction.csv'.")
+    # Delete the original downloaded file
+    os.remove(DownloadedFilename)
+
+print("Download completed...",downloadDir+'auction.csv')
 
