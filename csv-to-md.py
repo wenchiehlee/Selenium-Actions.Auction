25aa49d0ddf1b4f433a714447de7fe622690a54e
wenchiehlee <wjlee@csie.nctu.edu.tw>
2024-11-09 18:03:54 +0800

    1. use csv2md 2. change to use requirements.txt

--- csv-to-md.py
+++ csv-to-md.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+from csv2md.table import Table
 import sys
 
 def read_file(name, encoding='Big5'):
     try:
         with open(name, 'r', encoding=encoding) as file:
-            content = file.read()
-            print(content)
+            table = Table.parse_csv(file,columns=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])
+            print(table.markdown())
     except FileNotFoundError:
         print(f"File '{name}' not found.")
     except UnicodeDecodeError:
